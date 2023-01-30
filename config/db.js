592452927c1d132caa91fae04e41f892358dc1d2
farhanahzan <41515998+farhanahzan@users.noreply.github.com>
2023-01-30 11:49:02 +0530

    .env file created

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+//const config = require('config');
+const URL = require('dotenv').config();
+
+const db = process.env.MONGO_URL;
 
 const connectDB = async () => {
   try {
